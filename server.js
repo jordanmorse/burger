fac098d78020e09f3ed52991ba13067e9cd14814
Jordan Morse <jtm6496@gmail.com>
2020-11-20 09:42:06 -0500

    setting up server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express")
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, function() {
+    console.log("listening at port " + PORT);
+  });
