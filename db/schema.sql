434d7b78d2ca05869fcbe9d9aab5c4b1337d0cef
Jordan Morse <jtm6496@gmail.com>
2020-11-20 11:11:17 -0500

    setting up sql schema

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+drop database if exists burgers_db;
+
+create database burgers_db;
+
+use burgers_db;
+
+create table burgers (
+    id: int auto_increment not null,
+    burger_name: varchar(30) not null,
+    devoured: boolean not null,
+    primary key id
+);
